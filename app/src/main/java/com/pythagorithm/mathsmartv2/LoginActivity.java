688db1a7bbae3fa42a50cc212113a0b4a928313b
amrthejundi <amr.jundi@gmail.com>
2017-11-01 18:17:09 +0400

    Added firebase auth dependencies and google.json file

--- app/src/main/java/com/pythagorithm/mathsmartv2/LoginActivity.java
+++ app/src/main/java/com/pythagorithm/mathsmartv2/LoginActivity.java
@@ -3,8 +3,15 @@ package com.pythagorithm.mathsmartv2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
